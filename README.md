ebe3651b3943bd4f2dd8b086f50c505534d20498
me05458 <193655209+me05458@users.noreply.github.com>
2025-02-13 07:39:31 -0700

    Fixed incorrect random generator range and added game modes

--- README.md
+++ README.md
@@ -5,9 +5,13 @@ Game for learning words and vocabulary.\
 To run:
 ```git clone https://github.com/me05458/misc-scripts&&cd misc-scripts```\
 the words and answers need to be in two separate files, separated only by new lines. Add the file paths of the files to the config file, and make sure the first number in the config file is the number of words.\
+Modes:
+- 0 - displays a definition and you have to write a word.
+- 1 - displays a word and you have to write a definition.
 Usage:
-- Just `./WOTD.sh` will give definitions and you have to enter words. `quit` instead of a word will quit.
+- Just `./WOTD.sh` will have you play the game. `quit` or `q` instead of a word will quit.
 - `./WOTD.sh -a <word> <definition>` will add a word with a definition.
 - `./WOTD.sh -s w <file>` and `./WOTD.sh -s a <file>` will set filepath for words and defintions respectively.
+- `./WOTD.sh -s m <mode>` will set a mode.
 # dates
 So far only isLeapYear and getDaysShort work.
--- WOTD.sh
+++ WOTD.sh
@@ -5,6 +5,7 @@ answerpath=$(sed -n '2p' "$configpath")
 wordpath=$(sed -n '3p' "$configpath")
 RANDOM=$$
 wordnum=$(sed -n '1p' "$configpath")
+mode=$(sed -n '4p' "$configpath")
 
 i=2
 if [ "$1" == "-a" ]
@@ -15,34 +16,54 @@ if [ "$1" == "-a" ]
         echo "$wordnum" > "$configpath"
         echo "$answerpath" >> "$configpath"
         echo "$wordpath" >> "$configpath"
-        echo "Done!"
+        echo "$mode" >> "$configpath"
 elif [ "$1" == "-s" ]
     then
         if [ "$3" == "" ]
         then
-            echo "Please set a file name!"
+            echo "You have to actually set something!"
         else
             if [ "$2" == "a" ]
             then
                 echo "$wordnum" > "$configpath"
                 echo "$3" >> "$configpath"
                 echo "$wordpath" >> "$configpath"
+                echo "$mode" >> "$configpath"
+
             elif [ "$2" == "w" ]
             then
                 echo "$wordnum" > "$configpath"
                 echo "$answerpath" >> "$configpath"
-            echo "$3" >> "$configpath"
+                echo "$3" >> "$configpath"
+                echo "$mode" >> "$configpath"
+            elif [ "$2" == "m" ]
+            then
+                echo "$wordnum" > "$configpath"
+                echo "$answerpath" >> "$configpath"
+                echo "$wordpath" >> "$configpath"
+                echo "$3" >> "$configpath"
             else
-                echo "Please either set a (answer) or w (word)!"
+                echo "Please either set a (answer) or w (word) or m (mode)!"
             fi
         fi
 
 else
     while [ "$i" != 0 ]
     do
-        num=$(shuf --random-source='/dev/urandom' -n 1 -i 1-3)
-        word=$(head -"$num" "$wordpath" | tail -1)
-        answer=$(head -"$num" "$answerpath" | tail -1)
+    num=$(shuf --random-source='/dev/urandom' -n 1 -i 1-"$wordnum")
+    word=$(head -"$num" "$wordpath" | tail -1)
+    answer=$(head -"$num" "$answerpath" | tail -1)
+        if [ "$mode" == 1 ]
+        then
+            tmp="$word"
+            word="$answer"
+            answer="$tmp"
+        elif [ "$mode" != 0 ]
+            then
+             echo "Invalid mode detected. Setting to 0 (default)."
+        fi
+
+
         echo "$answer"
         read input
         if [ "$input" == "$word" ]
