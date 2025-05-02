fa1625fc9dfff1b028306299f1d5d30e9945daf7
me05458 <193655209+me05458@users.noreply.github.com>
2025-05-02 10:04:20 -0600

    put actual data in files :P

--- VPNCheck/setup.sh
+++ VPNCheck/setup.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+sudo chmod +x VPNcheck.sh
+sudo touch /lib/systemd/system/VPNCheck.service
+sudo cat setupdata > /lib/systemd/system/VPNCheck.service
+sudo systemctl daemon-reload
+sudo systemctl enable VPNCheck
+echo "All done!"
